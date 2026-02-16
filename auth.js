cd92da81b7852cdb1c89a543d334331811889de3
Dimasik <mitkogurkov562@gmail.com>
2026-02-16 18:23:31 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,70 @@
 function getUsers(){
-    return JSON.parse(localStorage.getItem("users")) || [];
+return JSON.parse(localStorage.getItem("users")) || [];
+}
+
+function saveUsers(users){
+localStorage.setItem("users", JSON.stringify(users));
 }
 
 function getCurrentUser(){
-    const username = localStorage.getItem("loggedUser");
-    if(!username) return null;
+const username = localStorage.getItem("loggedUser");
+if(!username) return null;
+return getUsers().find(u=>u.username===username);
+}
 
-    const users = getUsers();
-    return users.find(u => u.username === username);
+function register(e){
+e.preventDefault();
+
+const username = document.getElementById("username").value.trim();
+const password = document.getElementById("password").value.trim();
+
+let users = getUsers();
+
+if(users.find(u=>u.username===username)){
+showMsg("Потребителят съществува.");
+return;
+}
+
+users.push({
+username,
+password,
+role: username==="admin" ? "admin" : "user",
+blocked:false
+});
+
+saveUsers(users);
+localStorage.setItem("loggedUser", username);
+window.location="profile.html";
+}
+
+function login(e){
+e.preventDefault();
+
+const username = document.getElementById("username").value.trim();
+const password = document.getElementById("password").value.trim();
+
+let users = getUsers();
+let user = users.find(u=>u.username===username && u.password===password);
+
+if(!user){
+showMsg("Грешни данни.");
+return;
+}
+
+if(user.blocked){
+showMsg("Този акаунт е блокиран.");
+return;
+}
+
+localStorage.setItem("loggedUser", username);
+window.location="profile.html";
+}
+
+function logout(){
+localStorage.removeItem("loggedUser");
+window.location="index.html";
+}
+
+function showMsg(msg){
+document.getElementById("msg").innerText=msg;
 }
